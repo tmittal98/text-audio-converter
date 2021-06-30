935044618913328030b18e80963a240c590f4ed4
Tushar <tusharmittal429@gmail.com>
2021-06-30 12:21:10 +0530

    edited linkcomponent resultcomponent textcomponent

--- src/components/screens/resultcomponent.js
+++ src/components/screens/resultcomponent.js
@@ -1,32 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
-function ResultComponent() {
-    const readfile = () => {
-        var txtFile = new XMLHttpRequest();
-        txtFile.open("GET", "{url goes here}", true);
-        txtFile.onreadystatechange = function () {
-            if (txtFile.readyState === 4) {
-                if (txtFile.status === 200) {
-                    document.getElementById("div").innerHTML = txtFile.responseText;
-                }
-            }
-        };
-        txtFile.send(null);
-    };
+import axios from "axios";
+export default class ResultComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: [], audio: [] };
+  }
+  componentDidMount() {
+    axios
+      .get("url")
+      .then((response) =>
+        this.setState({ audio: response.data.audio, text: response.data.text })
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  textArea() {}
+  render() {
     return (
+      <div>
+        <h4 style={{ textAlign: "center" }}>
+          Text is Converted to your language with Audio
+        </h4>
+        <AudioPlayer
+          autoPlay
+          src={this.state.audio}
+          onPlay={(e) => console.log("onPlay")}
+        />
         <div>
-            <h4 style={{ textAlign: "center" }}>
-                Text is Converted to your language with Audio
-            </h4>
-            <AudioPlayer autoPlay src="" onPlay={(e) => console.log("onPlay")} />
-            <div onLoad="readfile();">
-                <form id="form1" runat="server">
-                    <div id="div"></div>
-                </form>
-            </div>
+          <p style={{ margin: "10 px 10px 10px 10px" }}>{this.state.text}</p>
         </div>
+      </div>
     );
+  }
 }
-
-export default ResultComponent;
--- src/components/screens/textcomponent.js
+++ src/components/screens/textcomponent.js
@@ -1,119 +1,114 @@
-import React, { useState, useRef, useEffect } from 'react';
-import M from 'materialize-css';
-
+import React, { useState, useRef, useEffect } from "react";
+import M from "materialize-css";
+import { Link } from "react-router-dom";
 function TestComponent() {
+  const dropDown = useRef(null);
+  const [text, setText] = useState("");
+  const [lang, setLang] = useState("");
 
-    const dropDown = useRef(null);
-    const [text, setText] = useState("");
-    const [lang, setLang] = useState("");
+  useEffect(() => {
+    M.Dropdown.init(dropDown.current);
+  }, []);
 
-    useEffect(() => {
-        M.Dropdown.init(dropDown.current);
-    }, []);
+  const postDetails = () => {
+    fetch("/text", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+        lang,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          M.toast({ html: data.error, classes: "#c62828 red darken-3" });
+        } else {
+          //
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const postDetails = () => {
-
-        fetch("/text", {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                text,
-                lang
-            })
-        }).then(res => res.json())
-            .then(data => {
-                if (data.error) {
-                    M.toast({ html: data.error, classes: "#c62828 red darken-3" })
-                }
-                else {
-                    //
-                }
-            }).catch(err => {
-                console.log(err)
-            });
-    }
-
-    return (
-        <div className="link-main-container">
-
-            <div className="card">
-
-                <div className="card-heading">
-                    <p>Paste your text here</p>
-                </div>
-
-                <div className="link-input">
-                    <textarea
-                        type="text"
-                        placeholder="paste your text here"
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                    />
-                    {/* <!-- Dropdown Trigger --> */}
-                    <button
-                        className='dropdown-trigger btn'
-                        data-target='dropdown1'
-                        ref={dropDown}
-                        value={lang}
-                        onChange={(e) => setLang(e.target.value)}
-                    >
-                        Drop Me!
-
-                    </button>
-
-                    {/* <!-- Dropdown Structure --> */}
-                    <ul id='dropdown1' className='dropdown-content'>
-                        <li className="list-item" >Bengali</li>
-                        <li className="list-item" >Arabic</li>
-                        <li className="list-item" >Czech</li>
-                        <li className="list-item" >Danish</li>
-                        <li className="list-item" >Dutch</li>
-                        <li className="list-item" >English</li>
-                        <li className="list-item" >English</li>
-                        <li className="list-item" >Finnish</li>
-                        <li className="list-item" >French</li>
-                        <li className="list-item" >German</li>
-                        <li className="list-item" >Greek</li>
-                        <li className="list-item" >Gujarati</li>
-                        <li className="list-item" >Hindi</li>
-                        <li className="list-item" >Hungarian</li>
-                        <li className="list-item" >Indonesian</li>
-                        <li className="list-item" >Italian </li>
-                        <li className="list-item" >Japanese</li>
-                        <li className="list-item" >Kannada</li>
-                        <li className="list-item" >Korean</li>
-                        <li className="list-item" >Malayalam </li>
-                        <li className="list-item" >Polish</li>
-                        <li className="list-item" >Portuguese </li>
-                        <li className="list-item" >Romanian</li>
-                        <li className="list-item" >Russian</li>
-                        <li className="list-item" >Slovak</li>
-                        <li className="list-item" >Spanish</li>
-                        <li className="list-item" >Spanish</li>
-                        <li className="list-item" >Swedish</li>
-                        <li className="list-item" >Tamil</li>
-                        <li className="list-item" >Turkish</li>
-                        <li className="list-item" >Ukrainian</li>
-                        <li className="list-item" >Vietnamese</li>
-                        <li className="list-item" >Telugu</li>
-                    </ul>
-                </div>
-
-                <div className="submit-button">
-                    <button
-                        className="waves-effect waves-light btn"
-                        onClick={() => postDetails()}
-                    >
-                        Click here to convert
-                    </button>
-                </div>
-
-            </div>
-
+  return (
+    <div className="link-main-container">
+      <div className="card">
+        <div className="card-heading">
+          <p>Paste your text here</p>
         </div>
-    );
+
+        <div className="link-input">
+          <textarea
+            type="text"
+            placeholder="paste your text here"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          {/* <!-- Dropdown Trigger --> */}
+          <button
+            className="dropdown-trigger btn"
+            data-target="dropdown1"
+            ref={dropDown}
+            value={lang}
+            onChange={(e) => setLang(e.target.value)}
+          >
+            Drop Me!
+          </button>
+
+          {/* <!-- Dropdown Structure --> */}
+          <ul id="dropdown1" className="dropdown-content">
+            <li className="list-item">Bengali</li>
+            <li className="list-item">Arabic</li>
+            <li className="list-item">Czech</li>
+            <li className="list-item">Danish</li>
+            <li className="list-item">Dutch</li>
+            <li className="list-item">English</li>
+            <li className="list-item">English</li>
+            <li className="list-item">Finnish</li>
+            <li className="list-item">French</li>
+            <li className="list-item">German</li>
+            <li className="list-item">Greek</li>
+            <li className="list-item">Gujarati</li>
+            <li className="list-item">Hindi</li>
+            <li className="list-item">Hungarian</li>
+            <li className="list-item">Indonesian</li>
+            <li className="list-item">Italian </li>
+            <li className="list-item">Japanese</li>
+            <li className="list-item">Kannada</li>
+            <li className="list-item">Korean</li>
+            <li className="list-item">Malayalam </li>
+            <li className="list-item">Polish</li>
+            <li className="list-item">Portuguese </li>
+            <li className="list-item">Romanian</li>
+            <li className="list-item">Russian</li>
+            <li className="list-item">Slovak</li>
+            <li className="list-item">Spanish</li>
+            <li className="list-item">Spanish</li>
+            <li className="list-item">Swedish</li>
+            <li className="list-item">Tamil</li>
+            <li className="list-item">Turkish</li>
+            <li className="list-item">Ukrainian</li>
+            <li className="list-item">Vietnamese</li>
+            <li className="list-item">Telugu</li>
+          </ul>
+        </div>
+
+        <div className="submit-button">
+          <Link
+            to="/result"
+            className="waves-effect waves-light btn"
+            onClick={() => postDetails()}
+          >
+            Click here to convert
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TestComponent;
